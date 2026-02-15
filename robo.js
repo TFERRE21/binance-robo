651024451e4f113c27bd6409bae33a09ec3d8e1d
TFERRE21 <tjesusferreira19@gmail.com>
2026-02-14 23:41:10 -0300

    fix api final

--- robo.js
+++ robo.js
@@ -3,7 +3,7 @@ const Binance = require('binance-api-node').default
 
 const client = Binance({
     apiKey: process.env.BINANCE_API_KEY,
-    apiSecret: process.env.BINANCE_SECRET_KEY,
+    apiSecret: process.env.BINANCE_API_SECRET,
     recvWindow: 60000
 })
 
